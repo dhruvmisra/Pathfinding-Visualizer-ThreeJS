2fdf7c5be63ba55dddcababec4ae86ca2b34b9cf
Dhruv <dhruvmisra@live.com>
2020-08-22 12:57:10 +0530

    Final optimized build

---
+++ dist/precache-manifest.6d8297ee15d4670d000164db3e3c4a61.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9eae0f2337bce81efac",
+    "url": "/Pathfinding-Visualizer-ThreeJS/css/app.d0fa1af5.css"
+  },
+  {
+    "revision": "d47983740cc9d1fa722fda8aea511a7e",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/building1.d4798374.png"
+  },
+  {
+    "revision": "ee52f081739634a05782e8ec9b42fdbc",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/building2.ee52f081.png"
+  },
+  {
+    "revision": "d6746a1097785098e1aa21578cc08cab",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/building3.d6746a10.png"
+  },
+  {
+    "revision": "6f16807f21cc804c7055a9eecf838b1f",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/cross.6f16807f.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "be570c434321c3a15e616d7e24908d01",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/ground-pebbles.be570c43.png"
+  },
+  {
+    "revision": "af02378ae987ce9944c923359b4e4737",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/ground.af02378a.png"
+  },
+  {
+    "revision": "da297261d5a5025cd484cbeaaaf72d7b",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/info.da297261.svg"
+  },
+  {
+    "revision": "c34f0813e9edd81b32e610445a518d15",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/maze.c34f0813.svg"
+  },
+  {
+    "revision": "f58e7b47fabfe168c20feb80948b1249",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/path.f58e7b47.svg"
+  },
+  {
+    "revision": "c00ad1c166ffea74d29d4c3abe1105fd",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/perspective.c00ad1c1.svg"
+  },
+  {
+    "revision": "08bbcff68d041ec04c756a1d18580b6f",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/reset-camera.08bbcff6.svg"
+  },
+  {
+    "revision": "a66ede3b3b6f1ed0fd0af1d39e08d73e",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/setup.a66ede3b.svg"
+  },
+  {
+    "revision": "6fd6fe5758a30356ffa2b6cf348e28fa",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/street-view.6fd6fe57.svg"
+  },
+  {
+    "revision": "6d02cd7c1029fc73e9c28fc81f89a713",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/firstPerson.jpg"
+  },
+  {
+    "revision": "e49e5880b211b5da5424ab5dec21a775",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/firstPerson2.jpg"
+  },
+  {
+    "revision": "3dd3f80a60e7b607f5e260c4bcb0abc8",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/maze.jpg"
+  },
+  {
+    "revision": "14cc3f96a30f9cdf8822447b4dee932e",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/maze2.jpg"
+  },
+  {
+    "revision": "7880f0b0616c8d317847dc74211ab8e4",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/road.png"
+  },
+  {
+    "revision": "b73230f268cd8cf6e1fa43ba62f971f0",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/selectAlgo.jpg"
+  },
+  {
+    "revision": "e4e5b83bdf81b4f153820fcfcc75a243",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/setupWorld.jpg"
+  },
+  {
+    "revision": "bcceefb2182f80a43049cde604f9c572",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/setupWorld2.jpg"
+  },
+  {
+    "revision": "5847f9ac04a518c81d7e3f57f671168c",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/setupWorld2.png"
+  },
+  {
+    "revision": "8fe9284f6bfcc0dd2badcda8946e95aa",
+    "url": "/Pathfinding-Visualizer-ThreeJS/img/tutorial/speed.jpg"
+  },
+  {
+    "revision": "661e1fcec92ea7f8b0485b00ca52cc4d",
+    "url": "/Pathfinding-Visualizer-ThreeJS/index.html"
+  },
+  {
+    "revision": "f9eae0f2337bce81efac",
+    "url": "/Pathfinding-Visualizer-ThreeJS/js/app.a48ddb12.js"
+  },
+  {
+    "revision": "cf5afb6019c793990550",
+    "url": "/Pathfinding-Visualizer-ThreeJS/js/chunk-vendors.c7dc677d.js"
+  },
+  {
+    "revision": "8c66aa81aac3177ee5e4ff60c69cd446",
+    "url": "/Pathfinding-Visualizer-ThreeJS/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Pathfinding-Visualizer-ThreeJS/robots.txt"
+  }
+]);
